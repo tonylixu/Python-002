c75cca51ac5887e3155a349144a8c60e03f29b8c
Tony Li Xu <tonylixu@gmail.com>
2020-07-28 20:58:14 -0400

    Add import DBConnector

--- week02/maoyanmovie/maoyanmovie/pipelines.py
+++ week02/maoyanmovie/maoyanmovie/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import pandas as pd
+from .utilities import DBConnector
 import yaml
 
 
