48abb1dfc519b0e6b65e708872ea6ff3412fd2a9
Tony Li Xu <tonylixu@gmail.com>
2020-10-11 10:27:49 -0400

    Update

--- week10/test.py
+++
@@ -1,8 +0,0 @@
-def check_subset(s):
-    size = len(s)
-    for i in range(size):
-        for j in range(i+1, size+1):
-            print(f's[i:j] = {s[i:j]}, s[i:j][::-1]={s[i:j][::-1]}')
-
-s = 'aaa'
-check_subset(s)
